1ba506e257a6d3f9b35032db70ae631279e2bab0
Selasie5 <selasisepenu5@gmail.com>
2025-09-04 18:02:59 +0000

    refactor: update landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,46 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${manrope.className} antialiased`}>
         {children}
-        <section className="grid grid-cols-1 md:grid-cols-3 w-full place-items-center p-10">
-          <div className="flex flex-col justify-center  items-center md:items-start gap-2">
-            <Image
-              src="/logo.png"
-              alt="Afrovivo Logo"
-              width={100}
-              height={200}
-            />
-            <h3 className="text-2xl font-semibold clash">
-              Let’s Power the Future, Together
-            </h3>
-          </div>
-          <div className="flex flex-col justify-center items-start gap-2">
-            <span className="text-gray-800 font-medium text-sm clash">
-              Accra, Ghana
-            </span>
-            <span className="text-gray-800 font-medium text-sm clash">
-              admin@afrovivo.com
-            </span>
-            <span className="text-gray-800 font-medium text-sm clash">
-              +233 553 086 503
-            </span>
-          </div>
-          <div className="flex justify-center items-start gap-2">
-            <span className="text-gray-800 font-medium text-sm clash">
-              <Link href="https://www.instagram.com/afrovivo_energy?igsh=cHFyNHNocHpzOTM3">
-                Instagram
-              </Link>
-            </span>
-            <span className="text-gray-800 font-medium text-sm clash">
-              <Link href="https://www.linkedin.com/company/afrovivo-energy/">
-                Linkedin
-              </Link>
-            </span>
-            <span className="text-gray-800 font-medium text-sm clash">
-              <Link href="">Twitter</Link>
-            </span>
-          </div>
-        </section>
-      </body>
+       </body>
     </html>
   );
 }
