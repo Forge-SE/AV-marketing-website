1a6ee3eb8914d24e3543b460073f22e6651e9f49
Selasie5 <selasisepenu5@gmail.com>
2025-09-20 14:56:56 +0000

    refactor: remove unused useEffect import from page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import Image from "next/image";
 import { ArrowRightIcon, BanknotesIcon, BriefcaseIcon, BuildingLibraryIcon, ChartBarIcon, ClockIcon, GlobeAltIcon, LinkIcon, UsersIcon } from "@heroicons/react/24/outline";
 import { TeamCard } from "@/components/TeamCard";
