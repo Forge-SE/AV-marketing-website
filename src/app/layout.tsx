729beb1b38e2a19421c4e9bb9b0fa280255c448e
Selasie Sepenu <73952162+Selasie5@users.noreply.github.com>
2025-09-20 16:00:53 +0000

    Feat:Add analytics (#12)
    * feat:update
    
    * feat: add responsive layout and navigation components for Energy Fund page
    
    * feat: enhance Energy Fund page navigation with section IDs and dropdown for sub-items
    
    * feat: enhance Navbar component with structured sub-menu for About and Energy Fund sections
    
    * feat: add Reveal component for scroll-based animations
    
    - Implemented a new Reveal component that uses Intersection Observer to trigger animations when elements come into view.
    - Supports customizable tag, className, delay, and id props.
    - Includes basic styles for reveal animations.
    
    * refactor: remove unused useEffect import from page component
    
    * feat: add @vercel/analytics for enhanced tracking and analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import {Footer} from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
+
+import { Analytics } from "@vercel/analytics/next"
+
 // import Link from "next/link";
 
 // const manrope = Manrope({
@@ -28,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+      <Analytics />
+
       <body className={` antialiased `} >
         <Navbar/>
         {children}
